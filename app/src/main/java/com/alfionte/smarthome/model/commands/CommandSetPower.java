d7e0ac1706f3f1faf4d3e32fdda4be29f68c1687
Gabriele Porcelli <gabriele.porcelli@scmitaly.it>
2017-12-12 15:00:36 +0100

    Add power mode

--- app/src/main/java/com/alfionte/smarthome/model/commands/CommandSetPower.java
+++ app/src/main/java/com/alfionte/smarthome/model/commands/CommandSetPower.java
@@ -10,14 +10,37 @@ public class CommandSetPower extends Command {
     public CommandSetPower(int id,
                            boolean isOn,
                            SpeedEffect speedEffect,
-                           @IntRange(from = 30) int duration) {
+                           @IntRange(from = 30) int duration,
+                           PowerMode powerMode) {
         super(id, "set_power");
 
         List<String> params = new ArrayList<>();
         params.add(isOn ? "on" : "off");
         params.add(speedEffect.toString());
         params.add(String.valueOf(duration));
+        params.add(String.valueOf(powerMode.getValue()));
 
         setParams(params);
+    }
+
+
+    public enum PowerMode {
+
+        NORMAL(0),  // default
+        COLOR_TEMPERATURE(1),
+        COLOR_RGB(2),
+        COLOR_HSV(3),
+        COLOR_FLOW(4),
+        NIGHT_LIGHT(5);
+
+        private int mValue;
+
+        PowerMode(int value) {
+            mValue = value;
+        }
+
+        public int getValue() {
+            return mValue;
+        }
     }
 }
